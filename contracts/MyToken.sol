5bb68297820d70fcd81cc030cab05d5365e4a33e
Amelia Wibisono <ameliawibi@gmail.com>
2022-09-18 20:56:36 +0800

    PAY-02 bulk transfer contract

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20{
-    constructor() ERC20("DevToken", "DVT"){
-        _mint(msg.sender,1000*10**18);
+    constructor() ERC20("MyToken", "MYT"){
+        _mint(msg.sender,1000000000*10**18);
     }
+
+function multiTransfer(address[] calldata _addresses, uint[] calldata _amounts) payable public returns (bool success) {
+    for (uint i = 0; i < _addresses.length; i++) {
+    transfer(_addresses[i], _amounts[i]);
+    }
+    return true;
+}
 }
