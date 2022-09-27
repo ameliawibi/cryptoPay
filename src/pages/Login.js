1165020e0b362421a01870e11ceb1be914c4df30
bernice <bernice@DESKTOP-Q9H0EBA.localdomain>
2022-09-28 00:26:32 +0800

    new image

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,12 @@ export default function LoginPage() {
   return (
     <>
       <Box position={"relative"}>
-        <Image w={"100%"} h={"100vh"} objectFit="fill" src={landingImage} />
+        <Image
+          w={"100%"}
+          h={"100vh"}
+          objectFit="fill"
+          src="https://img.freepik.com/free-vector/cryptocurrency-bitcoin-golden-coin-with-digital-circuit-lines-background_1017-33592.jpg?w=1380&t=st=1664295865~exp=1664296465~hmac=81f7985e8d03eadbbb1f94ffefd4694cddbca47c7b3060d7b343f5bb708c338c"
+        />
       </Box>
       {/* <Flex w={"100%"} h={"100vh"}> */}
       <Flex
@@ -15,7 +20,7 @@ export default function LoginPage() {
         alignContent="center"
         flexDirection={"column"}
         position="absolute"
-        right={"10%"}
+        left={"10%"}
         top={"35%"}
       >
         <Box>
