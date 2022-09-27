4494b1b1e5e82483dd4a0d6dcd7ac2a861f4ed58
bernice <bernice@DESKTOP-Q9H0EBA.localdomain>
2022-09-28 00:15:20 +0800

    loginpage

---
+++ src/components/templates/LoginForm.js
@@ -0,0 +1,79 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Box,
+  Center,
+} from "@chakra-ui/react";
+
+export default function LoginForm() {
+  //const {user, signIn} = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const origin = location.state?.from?.pathname || "/employees";
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    //signIn(email,password);
+    navigate(origin);
+  };
+
+  return (
+    <Center w={"100%"}>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ width: "90%" }}>
+        <Box mb={3} w={"100%"}>
+          <FormControl isInvalid={errors.email}>
+            <FormLabel color={"white"}>Email</FormLabel>
+            <Input
+              name="email"
+              focusBorderColor={"white"}
+              colorScheme={"white"}
+              color="white"
+              {...register("email", {
+                required: { value: true, message: "This is required" },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+        </Box>
+        <Box mb={3} w={"100%"}>
+          <FormControl isInvalid={errors.password}>
+            <FormLabel color={"white"}>Password</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              focusBorderColor={"white"}
+              colorScheme={"white"}
+              color="white"
+              {...register("password", {
+                required: { value: true, message: "This is required" },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
+        </Box>
+        <Box mt={6} w={"100%"}>
+          <Button type="submit" colorScheme="yellow" variant={"solid"}>
+            Login
+          </Button>
+        </Box>
+      </form>
+    </Center>
+  );
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,60 +1,28 @@
-import { useNavigate, useLocation } from "react-router-dom";
-import { useForm } from "react-hook-form";
-//import { useAuth } from "../hooks/useAuth";
-import { Button } from "@chakra-ui/react";
+import { Center, Flex, Image, Box } from "@chakra-ui/react";
+import LoginForm from "../components/templates/LoginForm";
+import landingImage from "../landingImage.jpg";
 
-export default function Login() {
-  //const {user, signIn} = useAuth();
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const origin = location.state?.from?.pathname || "/employees";
-
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-
-  const onError = (err, e) => {
-    console.log(err, e);
-  };
-
-  const onSubmit = (data) => {
-    console.log(data);
-    //signIn(email,password);
-    navigate(origin);
-  };
-
+export default function LoginPage() {
   return (
     <>
-      <h1>Login page</h1>
-
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <label>Email</label>
-        <input
-          name="email"
-          placeholder="Your email"
-          {...register("email", {
-            required: true,
-          })}
-        />
-        {errors.email && <p>This field is required</p>}
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          {...register("password", {
-            required: true,
-          })}
-        />
-        {errors.password && <p>This field is required</p>}
-        <br />
-        <Button type="submit" colorScheme="blue">
-          Login
-        </Button>
-      </form>
+      <Box position={"relative"}>
+        <Image w={"100%"} h={"100vh"} objectFit="fill" src={landingImage} />
+      </Box>
+      {/* <Flex w={"100%"} h={"100vh"}> */}
+      <Flex
+        w={"400px"}
+        justifyContent={"center"}
+        alignContent="center"
+        flexDirection={"column"}
+        position="absolute"
+        right={"10%"}
+        top={"35%"}
+      >
+        <Box>
+          <LoginForm />
+        </Box>
+      </Flex>
+      {/* </Flex> */}
     </>
   );
 }
