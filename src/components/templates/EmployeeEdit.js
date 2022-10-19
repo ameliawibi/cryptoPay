0bbe5b698ccc74fba33756ef8b8568b1cbe47e9c
Amelia Wibisono <ameliawibi@gmail.com>
2022-10-19 22:08:41 +0700

    finished employee view and edit

--- src/components/templates/EmployeeEdit.js
+++ src/components/templates/EmployeeEdit.js
@@ -1,4 +1,4 @@
-import { userData } from "../userData";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import {
   Input,
@@ -9,9 +9,30 @@ import {
   FormControl,
   Box,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { url } from "../../utils/url";
 
 export default function EmployeeEdit({ userId }) {
-  const user = userData.find((user) => user.id === Number(userId));
+  //const user = userData.find((user) => user.id === Number(userId));
+
+  const [user, setUser] = useState(null);
+
+  const initialize = async () => {
+    try {
+      const response = await axios.get(`${url}/employees`);
+      if (response) {
+        const allEmployees = response.data.allEmployees;
+
+        setUser(allEmployees.find((user) => user.id === Number(userId)));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    initialize();
+  }, []);
 
   const {
     register,
@@ -23,147 +44,158 @@ export default function EmployeeEdit({ userId }) {
     console.log(err, e);
   };
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    try {
+      let response = await axios.post(`${url}/update-employee/${userId}`, data);
+      if (response.status === 200) {
+        initialize();
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <Heading as="h4" size="md" color={"blue.800"} mb={6}>
-        Edit Account Information
-      </Heading>
+    <div>
+      {user && (
+        <main style={{ padding: "1rem 0" }}>
+          <Heading as="h4" size="md" color={"blue.800"} mb={6}>
+            Edit Account Information
+          </Heading>
 
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <Box mb={3} w={"100%"}>
-          <FormControl isInvalid={errors.name}>
-            <FormLabel
-              htmlFor="name"
-              id="name"
-              color={"blue.800"}
-              fontWeight="bold"
-            >
-              Name
-            </FormLabel>
-            <Input
-              type="text"
-              name="name"
-              w={"75%"}
-              defaultValue={user.name}
-              {...register("name", {
-                required: { value: true, message: "This is required" },
-              })}
-            />
-            <FormErrorMessage>
-              {errors.name && errors.name.message}
-            </FormErrorMessage>
-          </FormControl>
-        </Box>
+          <form onSubmit={handleSubmit(onSubmit, onError)}>
+            <Box mb={3} w={"100%"}>
+              <FormControl isInvalid={errors.name}>
+                <FormLabel
+                  htmlFor="name"
+                  id="name"
+                  color={"blue.800"}
+                  fontWeight="bold"
+                >
+                  Name
+                </FormLabel>
+                <Input
+                  type="text"
+                  name="name"
+                  w={"75%"}
+                  defaultValue={user.name}
+                  {...register("name", {
+                    required: { value: true, message: "This is required" },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.name && errors.name.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Box>
 
-        <Box mb={3} w={"100%"}>
-          <FormControl isInvalid={errors.email}>
-            <FormLabel
-              htmlFor="email"
-              id="email"
-              color={"blue.800"}
-              fontWeight="bold"
-            >
-              Email
-            </FormLabel>
-            <Input
-              type="text"
-              name="email"
-              w={"75%"}
-              defaultValue={user.email}
-              {...register("email", {
-                required: { value: true, message: "This is required" },
-              })}
-            />
-            <FormErrorMessage>
-              {errors.email && errors.email.message}
-            </FormErrorMessage>
-          </FormControl>
-        </Box>
+            <Box mb={3} w={"100%"}>
+              <FormControl isInvalid={errors.email}>
+                <FormLabel
+                  htmlFor="email"
+                  id="email"
+                  color={"blue.800"}
+                  fontWeight="bold"
+                >
+                  Email
+                </FormLabel>
+                <Input
+                  type="text"
+                  name="email"
+                  w={"75%"}
+                  defaultValue={user.email}
+                  {...register("email", {
+                    required: { value: true, message: "This is required" },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Box>
 
-        <Box mb={3} w={"100%"}>
-          <FormControl isInvalid={errors.designation}>
-            <FormLabel
-              htmlFor="designation"
-              id="designation"
-              color={"blue.800"}
-              fontWeight="bold"
-            >
-              Designation
-            </FormLabel>
-            <Input
-              type="text"
-              name="designation"
-              w={"75%"}
-              defaultValue={user.designation}
-              {...register("designation", {
-                required: { value: true, message: "This is required" },
-              })}
-            />
-            <FormErrorMessage>
-              {errors.designation && errors.designation.message}
-            </FormErrorMessage>
-          </FormControl>
-        </Box>
+            <Box mb={3} w={"100%"}>
+              <FormControl isInvalid={errors.designation}>
+                <FormLabel
+                  htmlFor="designation"
+                  id="designation"
+                  color={"blue.800"}
+                  fontWeight="bold"
+                >
+                  Designation
+                </FormLabel>
+                <Input
+                  type="text"
+                  name="designation"
+                  w={"75%"}
+                  defaultValue={user.designation}
+                  {...register("designation", {
+                    required: { value: true, message: "This is required" },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.designation && errors.designation.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Box>
 
-        <Box mb={3} w={"100%"}>
-          <FormControl isInvalid={errors.salary}>
-            <FormLabel
-              htmlFor="salary"
-              id="salary"
-              color={"blue.800"}
-              fontWeight="bold"
-              w={"75%"}
-            >
-              Salary
-            </FormLabel>
-            <Input
-              type="number"
-              name="salary"
-              defaultValue={user.salary}
-              w={"75%"}
-              {...register("salary", {
-                required: { value: true, message: "This is required" },
-              })}
-            />
-            <FormErrorMessage>
-              {errors.salary && errors.salary.message}
-            </FormErrorMessage>
-          </FormControl>
-        </Box>
+            <Box mb={3} w={"100%"}>
+              <FormControl isInvalid={errors.salary}>
+                <FormLabel
+                  htmlFor="salary"
+                  id="salary"
+                  color={"blue.800"}
+                  fontWeight="bold"
+                  w={"75%"}
+                >
+                  Salary
+                </FormLabel>
+                <Input
+                  type="number"
+                  name="salary"
+                  defaultValue={user.salary}
+                  w={"75%"}
+                  {...register("salary", {
+                    required: { value: true, message: "This is required" },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.salary && errors.salary.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Box>
 
-        <Box mb={3} w={"100%"}>
-          <FormControl isInvalid={errors.wallet}>
-            <FormLabel
-              htmlFor="wallet"
-              id="wallet"
-              color={"blue.800"}
-              fontWeight="bold"
-            >
-              Wallet
-            </FormLabel>
-            <Input
-              type="text"
-              name="wallet"
-              defaultValue={user.wallet}
-              w={"75%"}
-              {...register("wallet", {
-                required: { value: true, message: "This is required" },
-              })}
-            />
-            <FormErrorMessage>
-              {errors.wallet && errors.wallet.message}
-            </FormErrorMessage>
-          </FormControl>
-        </Box>
+            <Box mb={3} w={"100%"}>
+              <FormControl isInvalid={errors.wallet}>
+                <FormLabel
+                  htmlFor="walletAddress"
+                  id="walletAddress"
+                  color={"blue.800"}
+                  fontWeight="bold"
+                >
+                  Wallet
+                </FormLabel>
+                <Input
+                  type="text"
+                  name="walletAddress"
+                  defaultValue={user.walletAddress}
+                  w={"75%"}
+                  {...register("walletAddress", {
+                    required: { value: true, message: "This is required" },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.walletAddress && errors.walletAddress.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Box>
 
-        <Button type="submit" mt={8} w={"150px"}>
-          Save
-        </Button>
-      </form>
-    </main>
+            <Button type="submit" mt={8} w={"150px"}>
+              Save
+            </Button>
+          </form>
+        </main>
+      )}
+    </div>
   );
 }
--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,73 +1,103 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { userData } from "../components/userData";
 import EmployeeTab from "../components/atoms/EmployeeTab";
 import EmployeeEdit from "../components/templates/EmployeeEdit";
-import PageHeader from "../components/atoms/PageHeader";
 import EmpOverview from "../components/templates/EmpOverview";
 import WalletInformation from "../components/templates/WalletInformation";
 import { Avatar, Box, Flex, Divider, Center } from "@chakra-ui/react";
+import axios from "axios";
+import { url } from "../utils/url";
 
 export default function Employee() {
-  // const [editForm, renderEditForm] = useState(false);
-  const [tab, setTab] = useState("Overview");
   let params = useParams();
-  const user = userData.find((user) => user.id === Number(params.employeeId));
+  const [tab, setTab] = useState("Overview");
+
+  const [user, setUser] = useState(null);
+
+  const initialize = async () => {
+    try {
+      const response = await axios.get(`${url}/employees`);
+      if (response) {
+        const allEmployees = response.data.allEmployees;
+
+        setUser(
+          allEmployees.find((user) => user.id === Number(params.employeeId))
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    initialize();
+  }, []);
 
   return (
-    <Box
-      minH={"95vh"}
-      h={"100%"}
-      w={"100%"}
-      bgGradient="linear(to-l, purple.800, blue.700)"
-      overflow={"hidden"}
-    >
-      <main style={{ padding: "1rem 0" }}>
+    <div>
+      {user && (
         <Box
-          boxShadow="md"
-          p="6"
-          w={"90%"}
-          margin={"auto"}
-          mt={"10"}
-          rounded="md"
-          bg="white"
-          minH={"75vh"}
+          minH={"95vh"}
           h={"100%"}
+          w={"100%"}
+          bgGradient="linear(to-l, purple.800, blue.700)"
+          overflow={"hidden"}
         >
-          <Flex gap={8}>
-            <Box alignContent={"center"} minWidth={"200px"}>
-              <Center>
-                <Avatar name={user.name} size="2xl" mb={6} />
-              </Center>
-              <Flex flexDirection={"column"}>
-                <EmployeeTab text={"Overview"} setTab={setTab} tab={tab} />
-                <EmployeeTab
-                  text={"Wallet Information"}
-                  setTab={setTab}
-                  tab={tab}
-                />
-                <EmployeeTab text={"Edit Account"} setTab={setTab} tab={tab} />
+          <main style={{ padding: "1rem 0" }}>
+            <Box
+              boxShadow="md"
+              p="6"
+              w={"90%"}
+              margin={"auto"}
+              mt={"10"}
+              rounded="md"
+              bg="white"
+              minH={"75vh"}
+              h={"100%"}
+            >
+              <Flex gap={8}>
+                <Box alignContent={"center"} minWidth={"200px"}>
+                  <Center>
+                    <Avatar name={user.name} size="2xl" mb={6} />
+                  </Center>
+                  <Flex flexDirection={"column"}>
+                    <EmployeeTab text={"Overview"} setTab={setTab} tab={tab} />
+                    <EmployeeTab
+                      text={"Wallet Information"}
+                      setTab={setTab}
+                      tab={tab}
+                    />
+                    <EmployeeTab
+                      text={"Edit Account"}
+                      setTab={setTab}
+                      tab={tab}
+                    />
+                  </Flex>
+                </Box>
+                <Divider orientation="vertical" h={"100%"} minH={"70vh"} />
+                <Box w={"100%"}>
+                  {tab === "Overview" && (
+                    <EmpOverview
+                      name={user.name}
+                      designation={user.designation}
+                      email={user.email}
+                    />
+                  )}
+                  {tab === "Wallet Information" && (
+                    <WalletInformation
+                      salary={user.salary}
+                      wallet={user.walletAddress}
+                    />
+                  )}
+                  {tab === "Edit Account" && (
+                    <EmployeeEdit userId={params.employeeId} />
+                  )}
+                </Box>
               </Flex>
             </Box>
-            <Divider orientation="vertical" h={"100%"} minH={"70vh"} />
-            <Box w={"100%"}>
-              {tab === "Overview" && (
-                <EmpOverview
-                  name={user.name}
-                  designation={user.designation}
-                  email={user.email}
-                />
-              )}
-              {tab === "Wallet Information" && (
-                <WalletInformation salary={user.salary} wallet={user.wallet} />
-              )}
-              {tab === "Edit Account" && (
-                <EmployeeEdit userId={params.employeeId} />
-              )}
-            </Box>
-          </Flex>
+          </main>
         </Box>
-      </main>
-    </Box>
+      )}
+    </div>
   );
 }
